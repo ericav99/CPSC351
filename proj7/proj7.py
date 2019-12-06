3d3755fada3fca164cb5cb143129d3e70b537c96
Eric Av <eric av>
2019-12-05 19:16:21 -0800

    second

--- proj7/proj7.py
+++ proj7/proj7.py
@@ -1,59 +1,40 @@
-from pip._vendor.distlib.compat import raw_input
 #By Eric Av
 #Github repo name: CPSC351
-#Python version 3.8
+# #Python version 3.8
+import sys
 
-true = 0
-def main():
-    global true
-    length = 0;
-    while(length==0):
-        transition = [[[3], [1]], [[2], [2]], [[3], [1]], [[3], [3]]]
-        booleanToStart = 1
-        #dfa transition of L2 = {w | every odd position of w is a 1}
-        print("Test values:")
-        print("1010... should be true")
-        print("10010... should be false")
-        print("01... should be false")
-        print("00... should be false")
-        print("110... should be false\n")
-        input = raw_input("Type in a string of 1's and 0's to see if it is an acceptable string: ")
-        print("Test values:")
-        print("1010... should be true")
-        print("10010... should be false")
-        print("01... should be false")
-        print("00... should be false")
-        print("110... should be false\n")
-        input = list(input)
-        for index in range(len(input)):
-            if input[index] == '0':
-                input[index] = '0'
-            if input[index] == '1':
-                input[index] = '1'
-            if input[index] != '0' and input[index] != '1': #error checking out of bounds
-                booleanToStart = 0
+#define the tuples
+alphabet = ['0','1']
+states = [0,1,2,3]
+start = 0;
+accept = [1,2]
 
-        final = "121"
-        start = 0
-        i = 0
-        if booleanToStart == 1:
-            simulate(transition, input, final, start, i)
-        if true == 0 or booleanToStart == 0:
-            print("Result with " + input.__str__() + ": rejected")
-        if true == 1:
-            print("Result with " + input.__str__() + ": accepted")
-        true = 0
+#define the transition states
+transition_states = {0:{'0':3, '1':1}, 1:{'0':2, '1':2}, 2:{'0':3, '1':1}, 3:{'0':3, '1':3}}
 
-def simulate(transition, input, final, state, i):
-    global true
-    for j in range (len(input)):
-        if[input[j] == '0' or input[j] == '1']:
-            for each in transition[state][int(input[j])]:
-                if each < 4:
-                    state = each
-                    if j == len(input)-1 and (str(state) in final):
-                        true = 1
-                    simulate(transition, input[i+1:], final, state, i)
+#Input and also functionality to exit with cntrl-c
+try:
+    input_string = str(input("Input string: "))
+except KeyboardInterrupt:
+    sys.exit()
 
-        i = i+1
-main()
+#loop that moves through each character in the given input_string
+while True:
+    current_state = start
+    for character in str(input_string):
+        if input_string.__len__() == 0:
+            current_state = 3
+        if not character in alphabet:
+            current_state = 3
+        else:
+            current_state = transition_states[current_state][character]
+    if current_state in accept:
+        print("Accepted")
+    else:
+        print("Rejected")
+
+    #Input also to exit with cntrl-c
+    try:
+        input_string = input("Input string: ")
+    except KeyboardInterrupt:
+        sys.exit()
